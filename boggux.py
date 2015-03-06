d1088655faeaaba360827d60e4ebca84a8fe23ed
David García Garzón <voki@canvoki.net>
2015-03-06 01:43:23 +0100

    Able to run it from pytho

--- boggux.py
+++ boggux.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-from PyQt5 import QtQuick, QtGui, QtCore
+from PyQt5 import QtQuick, QtWidgets, QtCore, QtQml
 import sys
 
 if __name__ == '__main__':
 	import sys
-	app = QtGui.QGuiApplication(sys.argv)
-	w = QtQuick.QQuickView(QtCore.QUrl('boggux.qml'))
+
+	app = QtWidgets.QApplication(sys.argv)
+	engine = QtQml.QQmlApplicationEngine('boggux.qml')
+	w = engine.rootObjects()[0]
 	w.show()
 	sys.exit(app.exec_())
--- boggux.qml
+++ boggux.qml
@@ -1,9 +1,9 @@
 import QtQuick 2.3
 import QtQuick.Layouts 1.1
-import QtQuick.Window 2.0
+import QtQuick.Window 2.2
 import QtQuick.Controls 1.2
-import QtQuick.Dialogs 1.2
 import QtQuick.Controls.Styles 1.2
+import QtQuick.Dialogs 1.2
 
 Window {
 	id: main
